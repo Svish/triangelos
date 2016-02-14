9e18f9aa37f07a0cd636bf0532dc310123ca6a9f
Torleif Berger <torleif@outlook.com>
2016-02-14 12:49:36 +0100

    Merged Languages into Translator, style adjust

--- src/Helper/Translator.php
+++ src/Helper/Translator.php
@@ -6,10 +6,21 @@
  */
 class Helper_Translator
 {
+	private $strings;
+	private $languages;
+
+
 	public function __construct()
 	{
 		$this->strings = require STRINGS;
+		$this->languages = parse_ini_file(CONFIG.'sites.ini', true, INI_SCANNER_RAW);
+		foreach($this->languages as $key => &$val)
+		{
+			$val['href'] = 'http://'.$key;
+			$val['host'] = $key;
+		}
 	}
+
 
 	public function __invoke($text, Mustache_LambdaHelper $helper = null)
 	{
@@ -17,5 +28,11 @@ class Helper_Translator
 			? $this->strings[$text]
 			: $text;
 		return $helper->render($text);
+	}
+
+
+	public function languages()
+	{
+		return array_values($this->languages);
 	}
 }
