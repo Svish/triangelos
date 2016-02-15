7fd233ea1395e3ea76347632093266d442d7290c
Torleif Berger <torleif@outlook.com>
2016-02-15 02:19:34 +0100

    Re-enable mustache caching

--- src/Mustache.php
+++ src/Mustache.php
@@ -5,7 +5,7 @@ class Mustache
 	public static function engine()
 	{
 		return new Mustache_Engine([
-			//'cache' => CACHE.__CLASS__,
+			'cache' => CACHE.__CLASS__,
 			'loader' => new MyLoader,
 			'partials_loader' => new MyPartialsLoader,
 			'pragmas' => [Mustache_Engine::PRAGMA_FILTERS],
