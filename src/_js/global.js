68496be7fc4e059e3918b07b7f5a2d84ad616908
Torleif Berger <torleif@outlook.com>
2016-02-15 03:13:09 +0100

    String.contains polyfill

--- src/_js/global.js
+++ src/_js/global.js
@@ -33,3 +33,11 @@ $(function()
 		.ajaxStart(NProgress.start)
 		.ajaxStop(NProgress.done);
 });
+
+
+
+if(typeof String.prototype.contains === 'undefined')
+String.prototype.contains = function(it)
+{
+	return this.indexOf(it) != -1;
+};
