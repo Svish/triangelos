5a0dff8c961d78e9900cc3ccae850e566af3ff9a
Torleif Berger <torleif@outlook.com>
2016-02-18 04:06:16 +0100

    Collapsing menu and style fixes

--- src/_js/global.js
+++ src/_js/global.js
@@ -1,27 +1,6 @@
 
 $(function()
 {
-	// Mark current menu item
-	$('a', '#header').each(function() {
-		var href = $(this).attr('href');
-		var current = Site.Url.Current.contains(href) || href.contains(Site.Url.Host);
-		$(this).toggleClass('current', current);
-	});
-
-	if(window.location.host == 'localhost' || true) // TODO: Remove true when proper hosted
-	$('a.language', '#language-selector')
-		.on('click', function()
-		{
-			var host = $(this).data('host');
-			var age = 24*60*60;
-			document.cookie = 'host='+host
-				+'; path='+Site.Url.Base
-				+'; max-age='+age;
-			window.location.reload();
-			return false;
-		})
-
-
 	// Configure NProgress
 	NProgress.configure({
 		parent: '#header',
---
+++ src/_js/navigation.js
@@ -0,0 +1,42 @@
+/**
+ * Handles navigation, TOC creation and soft scrolling.
+ */
+var Navigation = {
+
+	init: function()
+	{
+		// Menu toggle button
+		$('#menu-toggle')
+			.click(Navigation.onMenuToggle);
+
+
+		// Mark current menu item
+		$('a', '#header').each(function() {
+			var href = $(this).attr('href');
+			var current = Site.Url.Current.contains(href) || href.contains(Site.Url.Host);
+			$(this).toggleClass('current', current);
+		});
+
+
+		// Cookie language switcher for dev env
+		if(['localhost','triangelos.geekality.net'].indexOf(window.location.host) >= 0)
+		$('a.language', '#language-selector')
+			.on('click', function()
+			{
+				var host = $(this).data('host');
+				var age = 24*60*60;
+				document.cookie = 'host='+host
+					+'; path='+Site.Url.Base
+					+'; max-age='+age;
+				window.location.reload();
+				return false;
+			});
+	},
+
+	onMenuToggle: function()
+	{
+		$('#header').toggleClass('open');
+		return false;
+	},
+};
+$(Navigation.init);
