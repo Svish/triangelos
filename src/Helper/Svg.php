7deedc33122c3b1b778c718f1c7c86a309588a79
Torleif Berger <torleif@outlook.com>
2016-03-08 23:26:39 +0100

    Render SVG name before looking for file to allow dynamic name

--- src/Helper/Svg.php
+++ src/Helper/Svg.php
@@ -2,13 +2,20 @@
 
 /**
  * SVG importer for Mustache templates.
+ *
+ * If the name includes a ";" everything
+ * following it will be added to the <svg> 
+ * tag as attributes.
  */
 class Helper_Svg
 {
 	const DIR = DOCROOT.'_'.DIRECTORY_SEPARATOR.'icon'.DIRECTORY_SEPARATOR;
 
-	public function __invoke($name)
+	public function __invoke($name, Mustache_LambdaHelper $render = null)
 	{
+		if($render)
+			$name = $render($name);
+
 		$opt = explode(';', $name, 2);
 		
 		$svg = File::get(self::DIR.$opt[0].'.svg');
