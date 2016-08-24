1b294732b85bd93fa16b81de79bb41c873571478
Torleif Berger <torleif@outlook.com>
2016-08-24 02:14:17 +0200

    Comment

--- src/Helper/FirstLine.php
+++ src/Helper/FirstLine.php
@@ -2,12 +2,13 @@
 
 
 /**
- * First line.
+ * Returns the first line of text.
  */
 class Helper_FirstLine
 {
 	public function __invoke($text, Mustache_LambdaHelper $render = null)
 	{
+		// Get text up till first <br>
 		$text = explode('<br>', $text, 2)[0];
 		return $render ? $render($text) : $text;
 	}
